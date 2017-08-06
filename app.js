1f1dde4834b3e62052de599d1b0b66454db8cbee
Kaitlyn Brouwers <kaitlynnicoleb@hotmail.com>
2017-08-06 13:46:06 -0400

    large updates on JS code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ starwars.init = function(){
 	starwars.getStarships();
 }
 
-//on click of search button, make ajax request to access Starwars API data
+//on click of search button, make ajax request to access Starwars API data for people
 starwars.getStarwars = function(){
 	$('#go').on('click', function() {
 		//document.getElementById('searchInputs').style.display = 'none';
@@ -26,40 +26,47 @@ starwars.getStarwars = function(){
 
 
 starwars.display = function(data) {
-	$.each(data, function(index, Object) {
+	$.each(data, function(index, object) {
+		console.log('menu loop function');
 		let $this = this;
-		let character = Object.name;
 		if(data.length === 0) {
+			document.getElementById('resultsContainer').style.backgroundColor = 'white';
+	        document.getElementById('resultsContainer').style.border = '30px solid #f8d731'
 		    $('#resultsContainer').append(`<p>Sorry, no results found!</p>`);
 		} else {
-	        $.each(data, function(index, object) {
+	        //$.each(data, function(index, object) {
 	            const character = object.name;
+	            document.getElementById('resultsContainer').style.backgroundColor = 'white';
+	            document.getElementById('resultsContainer').style.border = '30px solid #f8d731';
 	            $('#resultsContainer').append(`
-	            	<h2>${character}</h2> 
-	            	<li><em><strong>Birth Year:</strong></em> ${object.birth_year}</li> 
-	            	<li><em><strong>Eye Color:</strong></em> ${object.eye_color}</li> 
-	            	<li><em><strong>Films:</strong></em> ${object.films}</li> 
-	            	<li><em><strong>Gender:</strong></em> ${object.gender}</li> 
-	            	<li><em><strong>Hair Colour:</strong></em> ${object.hair_color}</li> 
-	            	<li><em><strong>Height:</strong></em> ${object.height}</li> 
-	            	<li><em><strong>Homeworld:</strong></em> ${object.homeworld}</li> 
-	            	<li><em><strong>Mass:</strong></em> ${object.mass}</li> 
-	            	<li><em><strong>Skin Color:</strong></em> ${object.skin_color}</li> 
-	            	<li><em><strong>Species:</strong></em> ${object.species}</li> 
-	            	<li><em><strong>Starships:</strong></em> ${object.starships}</li> 
-	            	<li><em><strong>Vehicles:</strong></em> ${object.vehicles}</li>`); 
-
-	            //I think this is more what they're looking for - just a list of categories which will then direct you to the info of the category you click on in a new pge - if i can figure that out, use below list - if not, use above list
-
-	            // $('#resultsContainer').append(`<h2>${character}</h2> <li>Birth Year</li> <li>Eye Colour</li> <li>Films</li> <li>Gender</li> <li>Hair Colour</li> <li>Height</li> <li>Homeworld</li> <li>Mass</li> <li>Skin Colour</li> <li>Species</li> <li>Species</li> <li>Starships</li> <li>Vehicles</li>`)   
-	        });
+	            	<div class="queryResults">
+		            	<h2>${character}</h2> 
+		            	<ul><li><a href="#" class="clickMe"><em><strong>Birth Year:</strong></em> ${object.birth_year}</a></li> 
+		            	<a href="#" class="clickMe"><li><em><strong>Eye Color:</strong></em> ${object.eye_color}</a></li> 
+		            	<li><a href="#" class="clickMe"><em><strong>Films:</strong></em> ${object.films}</a></li> 
+		            	<li><a href="#" class="clickMe"><em><strong>Gender:</strong></em> ${object.gender}</a></li> 
+		            	<li><a href="#" class="clickMe"><em><strong>Hair Colour:</strong></em> ${object.hair_color}</a></li> 
+		            	<li><a href="#" class="clickMe"><em><strong>Height:</strong></em> ${object.height}</a></li> 
+		            	<li><a href="#" class="clickMe"><em><strong>Homeworld:</strong></em> ${object.homeworld}</a></li> 
+		            	<li><a href="#" class="clickMe"><em><strong>Mass:</strong></em> ${object.mass}</a></li> 
+		            	<li><a href="#" class="clickMe"><em><strong>Skin Color:</strong></em> ${object.skin_color}</a></li> 
+		            	<li><a href="#" class="clickMe"><em><strong>Species:</strong></em> ${object.species}</a></li> 
+		            	<li><a href="#" class="clickMe"><em><strong>Starships:</strong></em> ${object.starships}</a></li> 
+		            	<li><a href="#" class="clickMe"><em><strong>Vehicles:</strong></em> ${object.vehicles}</a></li></ul></div>`);
+		        $('.clickMe').on('click', function() {
+		        	let $this = this;
+		        	$('#resultsContainer').empty();
+		        	$('#resultsContainer').append($this);
+		        }) 
+	        //});
 		}
 	});
 }
 
-//on click of search button, make ajax request to access Starwars API data
+//on click of search button, make ajax request to access Starwars API data for planets
 starwars.getPlanets = function(){
 	$('#go').on('click', function() {
+		//document.getElementById('#searchInputs').style.display = 'none';
 		$('#resultsContainer').empty();
 		const usersInput = document.getElementById("searchMe").value;
 		const planets = document.getElementById('planets');
@@ -77,37 +84,48 @@ starwars.getPlanets = function(){
 
 
 starwars.displayPlanets = function(data) {
-	$.each(data, function(index, Object) {
+	console.log(data)
+	$.each(data, function(index, object) {
 		let $this = this;
 		//let planet = Object.homeworld;
 		if(data.length === 0) {
+			document.getElementById('resultsContainer').style.backgroundColor = 'white';
+	            document.getElementById('resultsContainer').style.border = '30px solid #f8d731'
 		    $('#resultsContainer').append(`<p>Sorry, no results found!</p>`);
 		} else {
-	        $.each(data, function(index, results) {
-	            const planet = results.name;
+	       // $.each(data, function(index, results) {
+	            //const planet = results.name;
+	            document.getElementById('resultsContainer').style.backgroundColor = 'white';
+	            document.getElementById('resultsContainer').style.border = '30px solid #d8000e'
 	            $('#resultsContainer').append(`
-	            	<div> 
-	            		<h2>${planet}</h2> 
-	            		<li><em><strong>Climate:</strong></em> ${results.climate}</li> 
-	            		<li><em><strong>Created:</strong></em> ${results.created}</li> 
-	            		<li><em><strong>Diameter:</strong></em> ${results.diameter}</li> 
-	            		<li><em><strong>Edited:</strong></em> ${results.edited}</li> 
-	            		<li><em><strong>Films:</strong></em> ${results.films}</li> 
-	            		<li><em><strong>Gravity:</strong></em> ${results.gravity}</li> 
-	            		<li><em><strong>Rotation Period:</strong></em> ${results.rotation_period}</li> 
-	            		<li><em><strong>Surface Water:</strong></em> ${results.surface_water}</li> 
-	            		<li><em><strong>Terrain:</strong></em> ${results.terrain}</li></div>`);
-	            //I think this is more what they're looking for - just a list of categories which will then direct you to the info of the category you click on in a new pge - if i can figure that out, use below list - if not, use above list
-
-	            // $('#resultsContainer').append(`<div> <h2>${planet}</h2> <li>Climate</li> <li>Creation</li> <li>Diamater</li> <li>Edited</li> <li>Films Planet Was In</li> <li>Gravity of Planet</li> <li>Rotation Period</li> <li>Surface Water</li> <li>Terrain</li> </div>`)    
-	        });
+	            	<div class="queryResults"> 
+	            		<ul>
+		            		<h2>${object.name}</h2> 
+		            		<li><a href="#" class="clickMe"><em><strong>Climate:</strong></em> ${object.climate}</a></li> 
+		            		<li><a href="#" class="clickMe"><em><strong>Created:</strong></em> ${object.created}</a></li> 
+		            		<li><a href="#" class="clickMe"><em><strong>Diameter:</strong></em> ${object.diameter}</a></li> 
+		            		<li><a href="#" class="clickMe"><em><strong>Edited:</strong></em> ${object.edited}</a></li> 
+		            		<li><a href="#" class="clickMe"><em><strong>Films:</strong></em> ${object.films}</a></li> 
+		            		<li><a href="#" class="clickMe"><em><strong>Gravity:</strong></em> ${object.gravity}</a></li> 
+		            		<li><a href="#" class="clickMe"><em><strong>Rotation Period:</strong></em> ${object.rotation_period}</a></li> 
+		            		<li><a href="#" class="clickMe"><em><strong>Surface Water:</strong></em> ${object.surface_water}</a></li> 
+		            		<li><a href="#" class="clickMe"><em><strong>Terrain:</strong></em> ${object.terrain}</a></li>
+		            	</ul>
+		            </div>`);
+	            $('.clickMe').on('click', function() {
+		        	let $this = this;
+		        	$('#resultsContainer').empty();
+		        	$('#resultsContainer').append($this);
+		        }) 
+	        //});
 		}
 	});
 }
 
-//on click of search button, make ajax request to access Starwars API data
+//on click of search button, make ajax request to access Starwars API data for starships
 starwars.getStarships = function(){
 	$('#go').on('click', function() {
+		//document.getElementById('#searchInputs').style.display = 'none';
 		$('#resultsContainer').empty();
 		const usersInput = document.getElementById("searchMe").value;
 		const starships = document.getElementById('starships');
@@ -125,60 +143,65 @@ starwars.getStarships = function(){
 
 
 starwars.displayStarships = function(data) {
-	$.each(data, function(index, Object) {
+	$.each(data, function(index, object) {
+		console.log('menu loop function');
 		let $this = this;
-		//let planet = Object.homeworld;
 		if(data.length === 0) {
+			document.getElementById('resultsContainer').style.backgroundColor = 'white';
+	        document.getElementById('resultsContainer').style.border = '30px solid #f8d731'
 		    $('#resultsContainer').append(`<p>Sorry, no results found!</p>`);
 		} else {
-	        $.each(data, function(index, results) {
-	            const starships = results.name;
-	            $('#resultsContainer').append(`
-	            	<div> 
-	            		<h2>${starships}</h2> 
-	            		<li><em><strong>MGLT:</strong></em> ${results.MGLT}</li> 
-	            		<li><em><strong>Cargo Capacity:</strong></em> ${results.cargo_capacity}</li> 
-	            		<li><em><strong>Consumables:</strong></em> ${results.consumables}</li> 
-	            		<li><em><strong>Cost in Credits:</strong></em> ${results.cost_in_credits}</li> 
-	            		<li><em><strong>Crew:</strong></em> ${results.crew}</li> 
-	            		<li><em><strong>Films:</strong></em> ${results.films}</li> 
-	            		<li><em><strong>Hyperdrive Rating:</strong></em> ${results.hyperdrive_rating}</li> 
-	            		<li><em><strong>Length:</strong></em> ${results.length}</li> 
-	            		<li><em><strong>Manufacturer:</strong></em> ${results.manufacturer}</li>
-	            		<li><em><strong>Max Atmosphering Speed:</strong></em> ${results.max_atmosphering_speed}</li>
-	            		<li><em><strong>Model:</strong></em> ${results.model}</li>
-	            		<li><em><strong>Passengers:</strong></em> ${results.passengers}</li>
-	            		<li><em><strong>Pilots:</strong></em> ${results.pilots}</li>
-	            		<li><em><strong>Starship Class:</strong></em> ${results.starship_class}</li>
-	            		</div>`);
-	            //I think this is more what they're looking for - just a list of categories which will then direct you to the info of the category you click on in a new pge - if i can figure that out, use below list - if not, use above list
-
-	            // $('#resultsContainer').append(`<div> <h2>${planet}</h2> <li>Climate</li> <li>Creation</li> <li>Diamater</li> <li>Edited</li> <li>Films Planet Was In</li> <li>Gravity of Planet</li> <li>Rotation Period</li> <li>Surface Water</li> <li>Terrain</li> </div>`)    
-	        });
+	        //$.each(data, function(index, results) {
+	        	$('#searchInputs').hide();
+	            //const starships = object.name;
+	            document.getElementById('resultsContainer').style.backgroundColor = 'white';
+	            document.getElementById('resultsContainer').style.border = '30px solid #f8d731'
+	            $('#resultsContainer').append(`<div class="queryResults"> 
+	            		<ul>
+		            		<h2>${object.name}</h2> 
+		            		<li><a href="#" class="clickMe"><em><strong>MGLT:</strong></em> ${object.MGLT}</a></li> 
+		            		<li><a href="#" class="clickMe"><em><strong>Cargo Capacity:</strong></em> ${object.cargo_capacity}</a></li> 
+		            		<li><a href="#" class="clickMe"><em><strong>Consumables:</strong></em> ${object.consumables}</a></li> 
+		            		<li><a href="#" class="clickMe"><em><strong>Cost in Credits:</strong></em> ${object.cost_in_credits}</a></li> 
+		            		<li><a object="#" class="clickMe"><em><strong>Crew:</strong></em> ${object.crew}</a></li>  
+		            		<li><a href="#" class="clickMe"><em><strong>Hyperdrive Rating:</strong></em> ${object.hyperdrive_rating}</a></li> 
+		            		<li><a href="#" class="clickMe"><em><strong>Length:</strong></em> ${object.length}</a></li> 
+		            		<li><a href="#" class="clickMe"><em><strong>Manufacturer:</strong></em> ${object.manufacturer}</a></li>
+		            		<li><a href="#" class="clickMe"><em><strong>Max Atmosphering Speed:</strong></em> ${object.max_atmosphering_speed}</a></li>
+		            		<li><a href="#" class="clickMe"><em><strong>Model:</strong></em> ${object.model}</a></li>
+		            		<li><a href="#" class="clickMe"><em><strong>Passengers:</strong></em> ${object.passengers}</a></li>
+		            		<li><a href="#" class="clickMe"><em><strong>Pilots:</strong></em> ${object.pilots}</a></li>
+		            		<li><a href="#" class="clickMe"><em><strong>Starship Class:</strong></em> ${object.starship_class}</a></li>
+		            	</ul>
+	            	</div>`); 
+	            	$('.clickMe').on('click', function() {
+			        	let $this = this;
+			        	$('#resultsContainer').empty();
+			        	$('#resultsContainer').append(`<li>${$this}</li>`);
+		        	})   
+	        //});
 		}
 	});
 }
+
+$('#refresh').on('click', function() {
+	location.href = location.href;
+})
 
 $(function(){
 	starwars.init();
 });
 
-//give user option to refresh page 
-$('#refresh').on('click', function(){
-	console.log(this)
-	location.href = location.href;
-});
-
 //smooth scroll so results display on screen in a more obvious manner
-// $("#submitButton").on('click', function() {
-//     $('html,body').animate({
-//         scrollTop: $("#resultsContainer").offset().top},
-//         'slow');
-// });
+$(".fa").on('click', function() {
+    $('html,body').animate({
+        scrollTop: $("#resultsContainer").offset().top},
+        'slow');
+});
 
 //problems:
 //1. people and planets both coming up even when only clicking on one radio button
 //2. need to figure out how to empty container wen new option is pressed
 //3. need to figure out how to stop user from being able to press 2 radio bttons
 //4, need to turn data that is in arrays (like in 'films') into actual film titles, not the url
-//5 get refresh button to work
+//5 get refresh butto to work
--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,10 @@ const browserSync = require("browser-sync").create();
 const reload = browserSync.reload;
 
 gulp.task("styles", () => {
-	return gulp.src("./dev/styles/**/*.scss")
+	return gulp.src("styles/*.scss")
 		.pipe(sass().on("error", sass.logError))
 		.pipe(concat("style.css"))
-		.pipe(gulp.dest("./public/styles"))
+		.pipe(gulp.dest("styles"))
 		.pipe(reload({stream: true}));
 });
 
@@ -32,7 +32,7 @@ gulp.task("scripts", () => {
 });
 
 gulp.task("watch", () => {
-	gulp.watch("./dev/styles/**/*.scss", ["styles"]);
+	gulp.watch("styles/**/*.scss", ["styles"]);
 	gulp.watch("./dev/scripts/main.js", ["scripts"]);
 	gulp.watch("*.html", reload);
 });
